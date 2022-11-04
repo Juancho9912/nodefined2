f556958c944056040404b504690a8ae93ca4b9aa
Juan David Valencia <juan.1701826096@ucaldas.edu.co>
2022-11-03 21:01:56 -0500

    boton login

--- nodefined/src/components/login/Login.jsx
+++ nodefined/src/components/login/Login.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import TextField from '@mui/material/TextField';
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import LoginIcon from '@mui/icons-material/Login';
+import './login.css';
 
 const Login = () => {
   return (
-    <div className='container-login'>
-         <TextField style={{margin: '2rem 0'}} id="standard-basic" label="Email" variant="standard" />
-         <TextField className='input' id="standard-basic" label="Contraseña" variant="standard" />
+    <div className="loginContainer">
+      <TextField
+        style={{ margin: "2rem 0" }}
+        id="standard-basic"
+        label="Email"
+        variant="standard"
+      />
+      <TextField
+        className="input"
+        id="standard-basic"
+        label="Contraseña"
+        variant="standard"
+      />
+      <div className="buttomContainer">
+        <Button variant="outlined" startIcon={<LoginIcon />}>
+          Ingresar
+        </Button>
+        <span className="link">¿No tienes cuenta?</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
